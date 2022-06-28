b7ead50457bad836bd7bbe9558283fbfc8ae7537
osama3meera <osamahasan3meera@gmail.com>
2022-06-28 22:29:21 +0300

    code added

--- app/src/main/java/com/example/head_first_app/MainActivity.kt
+++ app/src/main/java/com/example/head_first_app/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.head_first_app
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val findJuice = findViewById<Button>(R.id.findJuice)
+        val juiceColor = findViewById<Spinner>(R.id.juiceColor)
+        val color = juiceColor.selectedItem
+        val brand = findViewById<TextView>(R.id.brands)
+        val juiceList = getJuice(color.toString())
+        val juices = juiceList.reduce{str, item -> str + '\n' + item}
+
+        findJuice.setOnClickListener {
+            brand.text = juices
+        }
+
+    }
+    private fun getJuice(color: String): List<String>{
+        return when (color){
+            "Orange" -> listOf("Rani - Orange","Original - Orange","Raugh - Orange")
+            "Kiwi" -> listOf("Rani - Kiwi","Original - Kiwi","Raugh - Kiwi")
+            "Strawberry" -> listOf("Rani - Strawberry","Original - Strawberry","Raugh - Strawberry")
+            else -> listOf("Milk Shake","Kiwi and Strawberry","Lemon and Mint")
+        }
     }
 }
