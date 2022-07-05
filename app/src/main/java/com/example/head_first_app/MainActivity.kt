40f54533de733040e7022b2afe3fc35cf26a983a
osama3meera <osamahasan3meera@gmail.com>
2022-07-05 23:18:06 +0300

    btn changes the juice

--- app/src/main/java/com/example/head_first_app/MainActivity.kt
+++ app/src/main/java/com/example/head_first_app/MainActivity.kt
@@ -12,25 +12,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val findJuice = findViewById<Button>(R.id.findJuice)
-        val juiceColor = findViewById<Spinner>(R.id.juiceColor)
-        val color = juiceColor.selectedItem
-        val brand = findViewById<TextView>(R.id.brands)
-        val juiceList = getJuice(color.toString())
-        val juices = juiceList.reduce{str, item -> str + '\n' + item}
 
         findJuice.setOnClickListener {
+
+            val juiceColor = findViewById<Spinner>(R.id.juiceColor)
+            val color = juiceColor.selectedItem
+            val juiceList = getJuice(color.toString())
+            val juices = juiceList.reduce { str, item -> str + '\n' + item }
+            val brand = findViewById<TextView>(R.id.brands)
             brand.text = juices
         }
 
     }
-    private fun getJuice(color: String): List<String>{
-        return when (color){
-            "Orange" -> listOf("Rani - Orange","Original - Orange","Raugh - Orange")
-            "Kiwi" -> listOf("Rani - Kiwi","Original - Kiwi","Raugh - Kiwi")
-            "Strawberry" -> listOf("Rani - Strawberry","Original - Strawberry","Raugh - Strawberry")
-            else -> listOf("Milk Shake","Kiwi and Strawberry","Lemon and Mint")
+
+    private fun getJuice(color: String): List<String> {
+        return when (color) {
+            "Orange" -> listOf("Rani - Orange", "Original - Orange", "Raugh - Orange")
+            "Kiwi" -> listOf("Rani - Kiwi", "Original - Kiwi", "Raugh - Kiwi")
+            "Strawberry" -> listOf(
+                "Rani - Strawberry",
+                "Original - Strawberry",
+                "Raugh - Strawberry"
+            )
+            else -> listOf("Milk Shake", "Kiwi and Strawberry", "Lemon and Mint")
         }
     }
 }
